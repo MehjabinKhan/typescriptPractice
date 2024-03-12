fa68f529244e935c5f085db185e8ef520e9185a0
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-12 23:59:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var favoriteColor = "Red";
+console.log(favoriteColor);
+favoriteColor = "Black";
+console.log(favoriteColor);
+var fruit = "Mango";
+fruit = "STrawberry";
+console.log(fruit);
+//camelCase :
+var userName = "Mehjabin";
+console.log(userName);
+var age = 20;
+console.log(age);
+var isStudent = true;
+console.log(isStudent);
+//strongly typed syntax 
+var a = "universityOfCamerino";
+console.log(a);
+var b = 10;
+console.log(b);
+var c = false;
+console.log(c);
+//type inference 
+var e = "Canada"; //Niagara Fall
+console.log(e);
+var f = 80; //number of apples distributed
+console.log(f);
+var g = "True"; //approximately 24 hours in a day
+console.log(g);
+// let and const
+var myFirstYearPercentage = 80; //this value can change 
+console.log(myFirstYearPercentage);
+var myResultInMartic = 90; //this value can't change
+console.log(myResultInMartic);
---
+++ main.ts
@@ -0,0 +1,33 @@
+let favoriteColor = "Red";
+console.log(favoriteColor);
+favoriteColor = "Black";
+console.log(favoriteColor);
+let fruit = "Mango";
+fruit = "STrawberry";
+console.log(fruit);
+//camelCase :
+let userName : string = "Mehjabin";
+console.log(userName);
+let age : number = 20;
+console.log(age);
+let isStudent : boolean = true;
+console.log(isStudent);
+//strongly typed syntax 
+let a : string = "universityOfCamerino";
+console.log(a);
+let b : number = 10;
+console.log(b);
+let c : boolean = false;
+console.log(c);
+//type inference 
+let e = "Canada"; //Niagara Fall
+console.log(e);
+let f = 80; //number of apples distributed
+console.log(f);
+let g = "True"; //approximately 24 hours in a day
+console.log(g);
+// let and const
+let myFirstYearPercentage = 80; //this value can change 
+console.log(myFirstYearPercentage);
+const myResultInMartic : number = 90; //this value can't change
+console.log(myResultInMartic);
